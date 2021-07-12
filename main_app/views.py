c11d5375e24d163e9b3af05fc5e2286c99bd1a6d
Elli Raynai <elli@elliraynai.com>
2021-07-12 12:28:22 -0400

    created signup login controller

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 from django.http import HttpResponse
@@ -6,3 +8,18 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello, world. You're at the Seat Selector Home Page.")
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
