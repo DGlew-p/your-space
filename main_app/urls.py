43a10febca43755786eb018783aa0332bda17262
Elli Raynai <elli@elliraynai.com>
2021-07-13 13:06:43 -0400

    detail page for timeslot added

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,11 +8,15 @@ urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
     path('index/', views.index, name='index'),
     path('profile/<int:profile_id>/', views.profile_detail, name='detail'),
-    path('profile/<int:profile_id>/add_photo/', views.add_photo, name='add_photo'),
+    path('profile/<int:profile_id>/add_photo/',
+         views.add_photo, name='add_photo'),
     path('profile/create/', views.ProfileCreate.as_view(), name='profile_create'),
-    path('profile/<int:pk>/update/', views.ProfileUpdate.as_view(), name='profile_update'),
-    path('profile/<int:pk>/delete/', views.ProfileDelete.as_view(), name='profile_delete'),
+    path('profile/<int:pk>/update/',
+         views.ProfileUpdate.as_view(), name='profile_update'),
+    path('profile/<int:pk>/delete/',
+         views.ProfileDelete.as_view(), name='profile_delete'),
+    path('timeslot/<int:pk>/', views.TimeSlotDetail.as_view(),
+         name='timeslot_detail'),
 
 
 ]
-
