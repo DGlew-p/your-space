52d81b30f00a1bbbe71e95ef8d451b8a5d702cde
Idahosa Ogieva <ida@Idahosas-MacBook-Air.local>
2021-07-14 12:41:39 -0400

    add wednesday code

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     
     path('accounts/signup/', views.signup, name='signup'),
     path('accounts/login/', views.login_request, name ="login"),
-    path("user", views.userpage, name = "userpage"),
+    path("user/<int:user_id>/", views.userpage, name = "userpage"),
     path('user/<int:user_id>/edit/', views.profile_edit, name='profile_edit'),
     path('user/<int:user_id>/update/', views.profile_update, name='profile_update'),
     path('user/<int:user_id>/assoc_timeslot/<int:timeslot_id>/', views.assoc_timeslot, name='assoc_timeslot'),
