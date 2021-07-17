bbb98ca214a1d931c0dbfeda65ee4658d0b04f64
dGlew <darrenjglew@outlook.com>
2021-07-17 17:32:58 -0400

    fixed dbl book bug

--- seat_tracker/settings.py
+++ seat_tracker/settings.py
@@ -13,6 +13,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from django.contrib.messages import constants as messages
+import environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env()
 
 MESSAGE_TAGS = {
     messages.DEBUG: 'alert-secondary',
@@ -30,10 +37,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://git.heroku.com/your-spaces.git']
 
